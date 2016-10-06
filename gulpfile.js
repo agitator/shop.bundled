9e7c697b8b2ab087a102b248e638a09fba32d4ad
Peter Holzer <hpeter@agitator.com>
2016-10-06 16:02:10 +0200

    disable default bundles - no uglify and merge yet

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
 var gulp = require('gulp');
+// var copy = require('gulp-copy');
+// var less = require('gulp-less');
 var concat = require('gulp-concat');
 var runSequence = require('run-sequence');
 var sourcemaps = require('gulp-sourcemaps');
 
 var path_theme = './src/shop/bundled/resources';
+// var path_plonetheme = './npm_modules/plonetheme.barceloneta/plonetheme/barceloneta/theme';
 var remoteSrc = require('gulp-remote-src');
 var cleanCSS = require('gulp-clean-css');
 var uglify = require('gulp-uglify');
@@ -13,6 +16,57 @@ gulp.task('all', function (cb) {
     runSequence('jsbundle', 'cssbundle', cb);
 });
 
+// // RUN ALL in sequence
+// gulp.task('all', function(cb) {
+//     'use strict';
+//     runSequence('theme', 'plone', cb);
+// });
+
+// // RUN ALL THEME
+// gulp.task('theme', function(cb) {
+//     'use strict';
+//     runSequence(['lesslib', 'less', 'concatjs'], cb);
+// });
+
+// // RUN ALL PLONE
+// gulp.task('plone', function(cb) {
+//     'use strict';
+//     runSequence(['ploneless', 'plonejs', 'ploneimg', 'plonefonts'], cb);
+// });
+
+
+// // THEME TASKS
+// //
+// gulp.task('lesslib', function() {
+//     'use strict';
+//     return gulp.src([path_theme + '/less/libraries.less', ])
+//     .pipe(sourcemaps.init())
+//     .pipe(less())
+//     .pipe(sourcemaps.write('.'))
+//     .pipe(gulp.dest(path_theme + '/css'));
+// });
+
+// gulp.task('less', function() {
+//     'use strict';
+//     return gulp.src([
+//         path_theme + '/less/site-default.less',
+//         path_theme + '/less/site-aaf.less',
+//         path_theme + '/less/site-afo.less',
+//         path_theme + '/less/site-ahk.less',
+//         path_theme + '/less/site-arb.less',
+//         path_theme + '/less/site-aut.less',
+//         path_theme + '/less/site-hda.less',
+//         path_theme + '/less/site-ia.less',
+//         path_theme + '/less/site-ogfa.less',
+//         path_theme + '/less/site-orte.less',
+//         path_theme + '/less/site-vai.less',
+//         path_theme + '/less/site-zv.less',
+//     ])
+//     .pipe(sourcemaps.init())
+//     .pipe(less())
+//     .pipe(sourcemaps.write('.'))
+//     .pipe(gulp.dest(path_theme + '/css'));
+// });
 
 gulp.task('concatjs', function () {
     'use strict';
@@ -25,6 +79,11 @@ gulp.task('concatjs', function () {
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(path_theme));
 });
+
+// gulp.task('watch', function () {
+//     'use strict';
+//     gulp.watch('./schubiduquartett/Basic-Theme2/less/*.less', ['less', 'ploneless']);
+// });
 
 gulp.task('jsbundle', function () {
     'use strict';
@@ -40,9 +99,9 @@ gulp.task('jsbundle', function () {
         base: 'https://raw.githubusercontent.com/'
     })
         .pipe(uglify())
-        .pipe(sourcemaps.init())
+        // .pipe(sourcemaps.init())
         .pipe(concat('shop-bundle.js'))
-        .pipe(sourcemaps.write('.'))
+        // .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(path_theme));
 });
 
@@ -57,9 +116,57 @@ gulp.task('cssbundle', function () {
     ], {
         base: 'https://raw.githubusercontent.com/'
     })
-        .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(sourcemaps.init())
+        // .pipe(cleanCSS({compatibility: 'ie8'}))
+        // .pipe(sourcemaps.init())
         .pipe(concat('shop-bundle.css'))
-        .pipe(sourcemaps.write('.'))
+        // .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(path_theme));
 });
+
+// // PLONE INTEGRATION
+// //
+// gulp.task('ploneless', function() {
+//     'use strict';
+//     return gulp.src([
+//         path_plonetheme + '/less/libraries.less',
+//         path_plonetheme + '/less/site-default.less',
+//         path_plonetheme + '/less/site-aaf.less',
+//         path_plonetheme + '/less/site-afo.less',
+//         path_plonetheme + '/less/site-ahk.less',
+//         path_plonetheme + '/less/site-arb.less',
+//         path_plonetheme + '/less/site-aut.less',
+//         path_plonetheme + '/less/site-hda.less',
+//         path_plonetheme + '/less/site-ia.less',
+//         path_plonetheme + '/less/site-ogfa.less',
+//         path_plonetheme + '/less/site-orte.less',
+//         path_plonetheme + '/less/site-vai.less',
+//         path_plonetheme + '/less/site-zv.less',
+//     ])
+//     .pipe(sourcemaps.init())
+//     .pipe(less({
+//         paths: [path_theme + '/..', ] // one level up, to be able to reference 'Basic-Theme2'
+//     }))
+//     .pipe(sourcemaps.write('.'))
+//     .pipe(gulp.dest(path_plonetheme + '/css'));
+// });
+// gulp.task('plonejs', function() {
+//     'use strict';
+//     return gulp.src([path_theme + '/js/libraries.js', path_theme + '/js/main.js'])
+//     .pipe(copy(path_plonetheme + '/js', {
+//         prefix: 3
+//     }));
+// });
+// gulp.task('ploneimg', function() {
+//     'use strict';
+//     return gulp.src([path_theme + '/img/*'])
+//     .pipe(copy(path_plonetheme + '/img/', {
+//         prefix: 3
+//     }));
+// });
+// gulp.task('plonefonts', function() {
+//     'use strict';
+//     return gulp.src([path_theme + '/fonts/**/*'])
+//     .pipe(copy(path_plonetheme + '/fonts/', {
+//         prefix: 3
+//     }));
+// });
